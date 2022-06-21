497e162a0ebb155a9d665faeb1474db644eeafd3
Dawitlior <dawitlior777@gmail.com>
2022-06-21 20:20:37 +0300

    add

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-class User {
-  constructor(firsName, lastName, age) {
-    this.firsName = firsName;
-    this.lastName = lastName;
-    this.age = age;
-  }
-}
-class Student extends User {
-  constructor(firsName, lastName, age, grade) {
-    super(firsName, lastName, age);
+// class User {
+//   constructor(firsName, lastName, age) {
+//     this.firsName = firsName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
+// }
+// class Student extends User {
+//   constructor(firsName, lastName, age, grade) {
+//     super(firsName, lastName, age);
+//     this.grade = grade;
+//   }
+// }
+// const newUser = new Student();
+
+1 - 4;
+class Student {
+  constructor(name, grade, id) {
+    this.name = name;
     this.grade = grade;
+    this.id = id;
   }
 }
-const newUser = new Student();
+let firstShow = new Student("lior", 12, 344223456);
+let firstShow1 = new Student("lidor", 2, 325832456);
+console.log(firstShow, firstShow1);
+
+class CollegeStudent extends Student {
+  constructor(name, grade, id, hight) {
+    super(name, grade, id);
+    this.hight = hight;
+  }
+}
+let collStudent = new CollegeStudent("lior", 11, 233211457, 1.69);
+console.log(collStudent);
+
+class HighSchoolStudent extends Student {
+  constructor(name, grade, id, phone) {
+    super(name, grade, id);
+    this.phone = phone;
+  }
+}
+let highSchoolStu = new HighSchoolStudent("david",8,295643211,088977657);
+console.log(highSchoolStu);
