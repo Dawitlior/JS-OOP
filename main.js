5656db44290152c020623828eb6a87939ff42dd6
Dawitlior <dawitlior777@gmail.com>
2022-06-22 13:00:41 +0300

    add

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// class User {
-//   constructor(firsName, lastName, age) {
-//     this.firsName = firsName;
-//     this.lastName = lastName;
-//     this.age = age;
-//   }
-// }
-// class Student extends User {
-//   constructor(firsName, lastName, age, grade) {
-//     super(firsName, lastName, age);
-//     this.grade = grade;
-//   }
-// }
-// const newUser = new Student();
-
+class User {
+  constructor(firsName, lastName, age) {
+    this.firsName = firsName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+class Student1 extends User {
+  constructor(firsName, lastName, age, grade) {
+    super(firsName, lastName, age);
+    this.grade = grade;
+  }
+}
+const newUser = new Student1("john", "doe", 10, 10);
+//---------------------------------------------------------------
 1 - 13;
 class Student {
   constructor(name, grade, id) {
@@ -21,7 +21,7 @@ class Student {
     this.id = id;
   }
   printToLog() {
-console.log(this.name,this.grade,this.id)
+    console.log(`first name:${this.name}grade:${this.grade}id:${this.id}`);
   }
 }
 let firstShow = new Student("lior", 12, 344223456);
@@ -38,20 +38,26 @@ let collStudent = new CollegeStudent("lior", 11, 233211457, 1.69);
 console.log(collStudent);
 
 class HighSchoolStudent extends Student {
-  constructor(name, grade, id, phone,listGrades) {
+  constructor(name, grade, id, phone, listGrades) {
     super(name, grade, id);
     this.phone = phone;
     this.listGrades = listGrades;
   }
-  printToLog(){
+  printToLog() {
     console.log("hello");
-    super.printToLog();
+    return `${super.printToLog()},grade:${this.grade},listGrade${this.listGrades}`;
   }
 }
-let highSchoolStu = new HighSchoolStudent("david",8,295643211,088977657,[98,87,65,45,90,100]);
+let highSchoolStu = new HighSchoolStudent(
+  "david",
+  8,
+  295643211,
+  088977657,
+  [98, 87, 65, 45, 90, 100]
+);
 console.log(highSchoolStu);
 
 class JuniorHighSchool extends Student {}
-const dawit = new JuniorHighSchool("lior",12,314253665);
+const dawit = new JuniorHighSchool("lior", 12, 314253665);
 console.log(dawit);
 dawit.printToLog();
